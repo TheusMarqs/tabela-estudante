2fe5bd507e9cd9d3788b4d782c83d6d4e459a60f
TheusMarqs <matheusmarqsilva@gmail.com>
2023-05-11 23:19:18 -0300

    base projeto

--- src/app/estudante.service.ts
+++ src/app/estudante.service.ts
@@ -1,10 +1,29 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Estudantes } from './estudante';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstudanteService {
+  url = "http://localhost:3000/Estudantes";
 
-  constructor(private http : HttpClientModule) { }
+  constructor(private http: HttpClient) { }
+
+  getEstudante() : Observable<Estudantes[]> {
+    return this.http.get<Estudantes[]>(this.url);
+  }
+
+  save(estudante: Estudantes) : Observable<Estudantes> {
+    return this.http.post<Estudantes>(this.url, estudante);
+  } 
+
+  update(estudante: Estudantes) : Observable<Estudantes> {
+    return this.http.put<Estudantes>(`${this.url}/${estudante.id}`, estudante);
+  }
+
+  delete(estudante: Estudantes) : Observable<void> {
+    return this.http.delete<void>(`${this.url}/${estudante.id}`);
+  }
 }
--- src/app/estudantes/estudantes.component.ts
+++ src/app/estudantes/estudantes.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EstudanteService } from '../estudante.service';
+import { Estudantes } from '../estudante';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-estudantes',
   templateUrl: './estudantes.component.html',
   styleUrls: ['./estudantes.component.css']
 })
-export class EstudantesComponent {
+export class EstudantesComponent implements OnInit {
+  estudantes: Estudantes[] = [];
+  isEditing: boolean = false;
+  formGroupEstudante: FormGroup;
+
+  constructor(private estudanteService: EstudanteService, private formBuilder: FormBuilder) {
+    this.formGroupEstudante = this.formBuilder.group({
+      id: [''],
+      name: [''],
+      age: 0,
+      email: [''],
+      phone: ['']
+    });
+  }
+
+  save() {
+    if (this.isEditing) {
+      this.estudanteService.update(this.formGroupEstudante.value).subscribe({
+        next: () => {
+          this.loadEstudantes();
+          this.formGroupEstudante.reset();
+          this.isEditing = false;
+        }
+      })
+    }
+
+    else {
+      this.estudanteService.save(this.formGroupEstudante.value).subscribe({
+        next: data => {
+          this.estudantes.push(data);
+          this.formGroupEstudante.reset();
+        }
+      })
+    }
+  }
+
+  ngOnInit() : void {
+    this.loadEstudantes();
+  }
+
+  loadEstudantes() {
+    this.estudanteService.getEstudante().subscribe({
+      next: data => this.estudantes = data
+    });
+  }
+    
+  edit(estudante: Estudantes) {
+    this.formGroupEstudante.setValue(estudante);
+    this.isEditing = true;
+  }
+
+  delete(estudante: Estudantes) {
+    this.estudanteService.delete(estudante).subscribe({
+      next: () => this.loadEstudantes()
+    });
+  }
 
 }
